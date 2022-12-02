45509f4d13cd00f592157b311032e98504a6ff43
Dmytro <vdude112358@gmail.com>
2022-12-02 17:34:51 +0200

    Add task to lab-10

---
+++ lab-10/task.py
@@ -0,0 +1,17 @@
+"""
+~ Task 1 ~
+
+Title: Image parser
+Task: Write a program that downloads images (at least 10) from the given site (choose whatever you want).
+Note: use threads for this task (at least 3 threads should be spawned)
+**Bonus task: parse all the images from the site using bs4**
+
+
+~ Task 2 ~
+
+Title: File creator
+Task: Write a program that generates n processes (where n is the number of kernels in your PC),
+    each of them should create a .txt file and populate it with symbols
+    (you may choose quotes from the books or random letters, etc). At least 20000000 should be added to each file.
+Note: use processes for that task
+"""
