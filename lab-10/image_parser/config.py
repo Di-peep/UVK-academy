141e73139f406d7d4e5dcd6743647804aea808aa
Dmytro <vdude112358@gmail.com>
2022-12-03 00:40:49 +0200

    Add configuration file

---
+++ lab-10/image_parser/config.py
@@ -0,0 +1,9 @@
+base_url = 'https://comfy.ua/ua/smartfon/brand__samsung/?p='
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"
+}
+
+class_item_box = 'products-list-item products-catalog-grid__item products-list-item--grid'
+class_item_img = 'ci-sl__slide-img'
+class_item_info = 'products-list-item__info'
+class_item_name = 'products-list-item__name'
