2ad16c7657650385869938f6d6dad3967eff92d6
Dmytro <vdude112358@gmail.com>
2022-12-12 13:22:23 +0200

    L11: Add task to lab-11

---
+++ lab-11/task.py
@@ -0,0 +1,10 @@
+"""
+Title: Image parser pt.2
+
+Task: Write a program that downloads images (at least 10) from the given site (choose whatever you want).
+
+Note: Update your code to be asynchronous.
+    Measure the time of image parsing and display it.
+
+**Bonus task: parse all the images from the site using bs4**
+"""
