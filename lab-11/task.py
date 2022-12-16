918db738ab3550039bf99611f341bb4f5fc463a1
Dmytro <vdude112358@gmail.com>
2022-12-16 15:55:37 +0200

    L11: Add execution time for 2 parser versions

--- lab-11/task.py
+++ lab-11/task.py
@@ -7,4 +7,15 @@ Note: Update your code to be asynchronous.
     Measure the time of image parsing and display it.
 
 **Bonus task: parse all the images from the site using bs4**
+
+
+>> Threads version:
+--- Execution: 6.0763983726501465 seconds ---
+--- Execution: 6.0034265865177693 seconds ---
+--- Execution: 6.6501756079364283 seconds ---
+
+>> Async version:
+--- Execution: 2.653421372600906 seconds ---
+--- Execution: 2.362654210090637 seconds ---
+--- Execution: 2.703563606422109 seconds ---
 """
