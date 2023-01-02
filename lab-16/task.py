85e2f2c0424cd2f61d9bbd7f81226d064ffedb90
Dmytro <vdude112358@gmail.com>
2023-01-02 12:24:45 +0200

    L16: Add task to lab-16

---
+++ lab-16/task.py
@@ -0,0 +1,9 @@
+"""
+Title: Blog
+
+Task: Create the scheme for the Personal Blog.
+    Think of data model, entities(at least 4, but more is better).
+    Visualize the scheme using Entity Relationship (ER) Diagram.
+
+Result: https://dbdiagram.io/d/63b29ede7d39e42284e86abb
+"""
