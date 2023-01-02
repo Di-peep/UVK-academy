75130d79ab2dbb834c7164bd72c04c9df55b6985
Di-peep <52866436+Di-peep@users.noreply.github.com>
2023-01-02 20:51:58 +0200

    L17: Fix auto tabulation

--- lab-17/script_creating_tables.sql
+++ lab-17/script_creating_tables.sql
@@ -4,20 +4,20 @@ create schema if not exists `lab-17`;
 use `lab-17`;
 
 create table Post (
-	post_id INT PRIMARY KEY AUTO_INCREMENT,
+    post_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) UNIQUE,
     text TEXT NOT NULL,
     date DATE
 );
 
 create table User (
-	user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
     nickname VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE
 );
 
 create table Comment (
-	comment_id INT PRIMARY KEY AUTO_INCREMENT,
+    comment_id INT PRIMARY KEY AUTO_INCREMENT,
     post_id INT,
     user_id INT,
     comment TEXT NOT NULL,
@@ -27,12 +27,12 @@ create table Comment (
 );
 
 create table Tag (
-	tag_id INT PRIMARY KEY AUTO_INCREMENT,
+    tag_id INT PRIMARY KEY AUTO_INCREMENT,
     tag_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 create table Post_has_tag (
-	post_id INT,
+    post_id INT,
     tag_id INT,
     CONSTRAINT PK_Post_Tag PRIMARY KEY (post_id, tag_id),
     FOREIGN KEY (post_id) REFERENCES Post(post_id),
@@ -40,7 +40,7 @@ create table Post_has_tag (
 );
 
 create table File (
-	file_id INT PRIMARY KEY AUTO_INCREMENT,
+    file_id INT PRIMARY KEY AUTO_INCREMENT,
     post_id INT,
     filename VARCHAR(255) NOT NULL UNIQUE,
     alttext VARCHAR(255) NOT NULL,
