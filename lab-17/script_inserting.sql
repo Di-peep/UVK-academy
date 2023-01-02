6cadcaec934c216de38a0cdb3d00007016c9008f
Di-peep <52866436+Di-peep@users.noreply.github.com>
2023-01-02 20:50:56 +0200

    L17: Fix auto tabulation

--- lab-17/script_inserting.sql
+++ lab-17/script_inserting.sql
@@ -2,18 +2,18 @@
 
 insert into Post (title, text, date)
 values 
-	('My first post', 'Hello, my dear subscribers. Welcome to my blog. And bla-bla-bla..', '2023-01-01'),
-	('My second post', 'text-text-text', '2023-01-01'),
+    ('My first post', 'Hello, my dear subscribers. Welcome to my blog. And bla-bla-bla..', '2023-01-01'),
+    ('My second post', 'text-text-text', '2023-01-01'),
     ('My third post about news', 'news-news-news', curdate());
 
 insert into User (nickname, email)
 values 
-	('Randy', 'random@gmail.com'),
-	('Weirny', 'weird@gmail.com');
+    ('Randy', 'random@gmail.com'),
+    ('Weirny', 'weird@gmail.com');
 
 insert into Comment (post_id, user_id, comment, date)
 values
-	(1, 1, 'cool!', curdate()),
+    (1, 1, 'cool!', curdate()),
     (1, 2, 'Gratz :0', curdate()),
     (2, 1, 'uhoo!', curdate());
 
