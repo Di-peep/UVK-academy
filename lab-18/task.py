a8a054b6696d1d80a16d91e4d649138fecdbe980
Dmytro <vdude112358@gmail.com>
2023-01-03 16:40:54 +0200

    Add task to lab-18

---
+++ lab-18/task.py
@@ -0,0 +1,34 @@
+"""
+Title: Blog
+
+Task1: Add queries to retrieve the data from you database.
+
+The task is to write:
+- Noncorrelated and Correlated Subqueries in Select
+- Write at least one From clause with subquery in it
+- Write at least one Where clause with subquery in it
+- Use WITH clause (Common Table Expression)
+- Group the data by some field and filter it with Having clause
+- Use Order by
+- Use Limit
+
+Task2: Add queries that work with multiple table to retrieve the data from you database.
+
+The task is to use:
+- INNER, LEFT, RIGHT, OUTER joins
+- USING
+- NATURAL JOIN
+- CROSS JOIN
+- SELF JOIN
+- UNION
+- EXCEPT and INTERSECT
+
+Task3: Add statements to work with the db.
+
+The task is to use:
+- transactions (at least 2)
+- coalesce
+- cast
+- case when
+- use different datetime functions
+"""
