d97fe71efc772328b379f6aff8f16fae3f091a73
Dmytro <vdude112358@gmail.com>
2023-01-19 13:58:40 +0200

    L21: Fix api for pt1

--- lab-21/wsgi_pt_1/api.py
+++ lab-21/wsgi_pt_1/api.py
@@ -11,12 +11,6 @@ class App:
         start_response(response_status, response_headers)
         return [response_body.encode()]
 
-    # def __call__(self, environ, start_response):
-    #     response_body = b"Hello, World!"
-    #     status = "200 OK"
-    #     start_response(status, headers=[])
-    #     return iter([response_body])
-
 
 class FirstMiddleware:
     """
