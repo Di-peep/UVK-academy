1ecd4b762e9b7cab2588c3e75226399c712cb071
Dmytro <vdude112358@gmail.com>
2023-01-27 23:13:47 +0200

    L23: Add task to lab23

--- lab-22/task.py
+++ lab-22/task.py
@@ -1,6 +1,14 @@
 """
-Title: Django pt.2
+Title: Django pt.1
 
 Task: Implement the code from lecture 1.
     Read more about django.
+
+
+Title: Django pt.2
+
+Task: Implement the code from lecture 2.
+    Read more about django.
+
+    Add the Django debug toolbar to your project.
 """
